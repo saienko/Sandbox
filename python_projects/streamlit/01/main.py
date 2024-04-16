7003e5696cf6d873b32c0299e6dcc74a7b8c2a48
Saienko Andrii <62471179+saienko@users.noreply.github.com>
2024-04-16 12:21:52 +0300

    fix: (rename) "rename main streamlit file"

---
+++ python_projects/streamlit/01/main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+# header = st.beta_container()
+# dataset = st.beta_container()
+# features = st.beta_container()
+# modelTraining = st.beta_container()
+
+
+st.title('Uber pickups in NYC')
