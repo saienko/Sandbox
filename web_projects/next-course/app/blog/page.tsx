86747681140b0e411ad4ebe3295b5bde31e7f6cd
Saienko Andrii <62471179+saienko@users.noreply.github.com>
2024-04-29 14:42:32 +0300

    next course

---
+++ web_projects/next-course/app/blog/page.tsx
@@ -0,0 +1,3 @@
+export default function Blog() {
+  return <h1>Blof Page</h1>;
+}
---
+++ web_projects/next-course/app/not-found.tsx
@@ -0,0 +1,9 @@
+// Custom Page 404
+export default function NotFound() {
+  return (
+    <div>
+      <h2>Page not found</h2>
+      <p>could not find</p>
+    </div>
+  );
+}
