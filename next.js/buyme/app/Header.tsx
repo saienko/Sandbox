d50298f6c2b09f262f4857a781c17c4c523f5aa8
Saienko Andrii <62471179+saienko@users.noreply.github.com>
2024-09-05 13:13:30 +0300

    header

--- next.js/buyme/app/Header.tsx
+++ next.js/buyme/app/Header.tsx
@@ -1,11 +1,41 @@
 import Link from "next/link";
 import React from "react";
 
+const links = [
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "About",
+    href: "/about/",
+  },
+  {
+    title: "FAQ",
+    href: "/faq/",
+  },
+  {
+    title: "Login",
+    href: "/login/",
+  },
+];
 export default function Header() {
   return (
-    <header className="flex justify-between p-4 max-w-screen-xl">
-      <Link href={"/"}>Home</Link>
-      <nav></nav>
+    <header className="flex justify-between p-4 max-w-screen-xl border-b">
+      <div></div>
+      <nav className="flex gap-4 items-center">
+        {links.map((link, idx) => (
+          <Link href={link.href} key={idx}>
+            {link.title}
+          </Link>
+        ))}
+        <button className="border rounded-full px-4 py-2">
+          <Link href={"login"}>Sing In</Link>
+        </button>
+        <button className="bg-amber-400 rounded-full px-4 py-2 shadow-md hover:bg-amber-800 hover:text-white">
+          <Link href={"login"}>Sing Up</Link>
+        </button>
+      </nav>
     </header>
   );
 }
--- next.js/buyme/app/page.tsx
+++ next.js/buyme/app/page.tsx
@@ -5,7 +5,9 @@ export default function Home() {
     <main className="max-w-lg mx-auto text-center p-10">
       <h1 className="text-6xl font-bold">Buy me a...</h1>
       <h3 className="mt-4 mb-8">Assept support for a your work</h3>
-      <button className="bg-amber-500 px-4 py-4 font-bold rounded-full">Start my page</button>
+      <button className="bg-amber-500 px-4 py-4 font-bold rounded-full shadow-md">
+        Start my page
+      </button>
     </main>
   );
 }
