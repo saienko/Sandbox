fca997f543256e4f35dd65d6e118f840efcb6aac
Saienko Andrii <62471179+saienko@users.noreply.github.com>
2024-09-12 16:09:06 +0300

    Lists

--- next.js/assistant/app/components/dashboard/Dashboard.tsx
+++ next.js/assistant/app/components/dashboard/Dashboard.tsx
@@ -15,20 +15,7 @@ export default function Dashboard() {
                 Tuesday, Aug 8
               </span>
             </div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              className="size-6 absolute right-2"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5"
-              />
-            </svg>
+
           </button>
         </div>
       </div>
