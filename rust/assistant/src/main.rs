72dbefebc293bcd870564a9536d5e9a395770ba9
Saienko Andrii <62471179+saienko@users.noreply.github.com>
2024-09-24 11:13:49 +0300

    lib

--- rust/assistant/src/main.rs
+++ rust/assistant/src/main.rs
@@ -1,17 +1,23 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
-    let condition = false;
-    let number = if condition { 5 } else { 6 };
-
-    println!("\nThe value of number is: {number}\n");
-    let mut counter = 0;
-
-    let result = loop {
-        counter += 1;
-
-        if counter == 10 {
-            break counter * 2;
-        }
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
     };
 
-    println!("The result is {result}");
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
 }
